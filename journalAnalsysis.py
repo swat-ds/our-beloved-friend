930462dfacb76eb8189d636ed25b35f4bbcb5e8f
James Truitt <jtruitt1@swarthmore.edu>
2021-07-23 10:40:00 -0400

    Create initial top-down design of journalAnalysis.py

--- journalAnalsysis.py
+++ journalAnalsysis.py
@@ -4,3 +4,29 @@ A suite of programs to perform analysis on the TEI-markup Hunt Journals.
 James Truitt
 July 2021
 """
+
+def loadXml():
+	pass
+
+def getBoolCooccurrences(tei):
+	pass
+
+def getCountCoocurrences(tei):
+	pass
+
+def main():
+	# Load the TEI data from files
+	teis = loadXml()
+
+	# Initialize a holder for our data
+	data = []
+
+	# Loop over the TEI files
+	for journal in teis:
+		# From each TEI, extract set of sets of ARKids cooccurring in entries
+		collocs = getBoolCooccurrences(journal)
+
+		# Add that set to data holder
+		data += collocs
+
+main()
